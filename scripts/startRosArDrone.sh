9ed2b60c7f75ef3e7f038e7dec95c326ddd43dda
Christian Heinrich <heinrich.christian@gmx.net>
2014-12-30 19:52:56 -0500

    start rviz in script with new config

--- scripts/startRosArDrone.sh
+++ scripts/startRosArDrone.sh
@@ -7,5 +7,9 @@ xterm -hold -e rosrun image_view image_view image:=/ardrone/front/image_raw &
 sleep 1
 xterm -hold -e rostopic echo /ardrone/navdata  &
 sleep 1
-xterm -hold -e roslaunch ardrone_joystick teleop.launch 
-
+xterm -hold -e roslaunch ardrone_joystick teleop.launch &
+sleep 1
+xterm -hold -e rosrun ardrone_followme ardrone_followme &
+sleep 1
+xterm -hold -e rostopic echo /visualization_marker &
+xterm -hold -e rosrun rviz rviz -d  "../configs/ShowMarkerConfig.rviz"
