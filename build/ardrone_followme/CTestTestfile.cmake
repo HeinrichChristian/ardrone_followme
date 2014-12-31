85337d0de5f0a0e29f37554234926319d047da77
Christian Heinrich <heinrich.christian@gmx.net>
2014-12-31 01:10:54 +0100

    added .gitignore and deleted generated dirs and files

--- build/ardrone_followme/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/hydro/share/ardrone_followme/src/ardrone_followme
-# Build directory: /opt/ros/hydro/share/ardrone_followme/build/ardrone_followme
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /opt/ros/hydro/share/ardrone_followme/build/gtest
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
