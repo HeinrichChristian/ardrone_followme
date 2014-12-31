ad876f2c596b5a1722fcea86a1e4d917c930f06a
Christian Heinrich <heinrich.christian@gmx.net>
2014-12-31 05:47:06 -0500

    show camera and marker in rviz

--- scripts/startRosArDrone.sh
+++ scripts/startRosArDrone.sh
@@ -3,8 +3,6 @@ xterm -hold -e roscore &
 sleep 1
 xterm -hold -e roslaunch ardrone_autonomy ardrone.launch  &
 sleep 1
-xterm -hold -e rosrun image_view image_view image:=/ardrone/front/image_raw &
-sleep 1
 xterm -hold -e rostopic echo /ardrone/navdata  &
 sleep 1
 xterm -hold -e roslaunch ardrone_joystick teleop.launch &
--- src/ardrone_followme/ardrone_followme.cpp
+++ src/ardrone_followme/ardrone_followme.cpp
@@ -16,7 +16,7 @@ void poseMessageReceived ( const ardrone_autonomy::Navdata& msg) {
 	
 	    visualization_msgs::Marker marker;
 	    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-	    marker.header.frame_id = "/my_frame";
+	    marker.header.frame_id = "ardrone_base_frontcam";
 	    marker.header.stamp = ros::Time::now();
 	 
 	    // Set the namespace and id for this marker.  This serves to create a unique ID
@@ -31,8 +31,8 @@ void poseMessageReceived ( const ardrone_autonomy::Navdata& msg) {
 	    marker.action = visualization_msgs::Marker::ADD;
 
 		// Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-		marker.pose.position.x = msg.tags_xc[0] / 100;
-		marker.pose.position.y = msg.tags_yc[0] / 100;
+		marker.pose.position.x = msg.tags_xc[0] / 10;
+		marker.pose.position.y = msg.tags_yc[0] / 10;
 		marker.pose.position.z = 0;
 		marker.pose.orientation.x = 0.0;
 		marker.pose.orientation.y = 0.0;
@@ -40,9 +40,9 @@ void poseMessageReceived ( const ardrone_autonomy::Navdata& msg) {
 		marker.pose.orientation.w = 1.0;
   
 		// Set the scale of the marker -- 1x1x1 here means 1m on a side
-		marker.scale.x = 1.0;
-		marker.scale.y = 1.0;
-		marker.scale.z = 1.0;
+		marker.scale.x = 0.5;
+		marker.scale.y = 0.5;
+		marker.scale.z = 0.5;
   
 		// Set the color -- be sure to set alpha to something non-zero!
 		marker.color.r = 0.0f;
